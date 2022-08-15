ad43bfacd93d3ea8cc92097f6ea3f8662eb8c23e
lhk3337 <lhk3337@naver.com>
2022-08-15 13:55:40 +0900

    [FEAT] main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
 import React from "react";
 
-export default () => <div>z</div>;
+export default () => (
+  <div>
+    <h1>Welcome to (Name)!!</h1>
+    <p>Your email is: (Your eMail). </p>
+  </div>
+);
