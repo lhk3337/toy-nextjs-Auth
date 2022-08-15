ba2f3dc0fd373a4b8018d7ca87968db860312103
lhk3337 <lhk3337@naver.com>
2022-08-15 13:55:57 +0900

    [FEAT] routing page

---
+++ pages/create-account/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Create = () => {
+  return (
+    <>
+      <h1>Create Account</h1>
+      <form action="">
+        <div>
+          <span>Name:</span>
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <span>Email:</span>
+          <input type="email" />
+        </div>
+        <input type="submit" value="Create Account" />
+      </form>
+    </>
+  );
+};
+
+export default Create;
---
+++ pages/log-in/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="">
+        <div>
+          <span>Email:</span>
+          <input type="text" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
