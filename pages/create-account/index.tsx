71375aec1f770dee7f932b90fe19b6c3821c8c0e
lhk3337 <lhk3337@naver.com>
2022-08-16 10:04:04 +0900

    [feat] form react-hook-form 설정하기

--- pages/create-account/index.tsx
+++ pages/create-account/index.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 const Create = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IUser>();
+  const onSubmit: SubmitHandler<IUser> = (data) => {
+    console.log(data);
+  };
+  const onInValid = () => {
+    console.log("Invalid");
+  };
   return (
     <>
       <h1>Create Account</h1>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit, onInValid)}>
         <div>
           <span>Name:</span>
-          <input type="text" name="" id="" />
+          <input type="text" {...register("name", { required: true })} />
         </div>
         <div>
           <span>Email:</span>
-          <input type="email" />
+          <input type="email" {...register("email")} />
         </div>
         <input type="submit" value="Create Account" />
       </form>
--- pages/log-in/index.tsx
+++ pages/log-in/index.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+interface IUser {
+  email: string;
+}
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IUser>();
+  const onSubmit: SubmitHandler<IUser> = (data) => {
+    console.log(data);
+  };
+  const onInvalid = () => {
+    alert("이메일을 입력하세요");
+  };
   return (
-    <div>
+    <>
       <h1>Login</h1>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
         <div>
           <span>Email:</span>
-          <input type="text" />
+          <input type="email" {...register("email", { required: true })} />
         </div>
         <input type="submit" value="Login" />
       </form>
-    </div>
+    </>
   );
 };
 
